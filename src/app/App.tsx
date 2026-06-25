28f25bd287405f706ae783cee5f899bd29d77349
DenisKramarenko13 <cashflow.dkm@gmail.com>
2026-06-25 19:27:25 +1000

    Added chart.js

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,37 @@
+import { useEffect, useState } from 'react';
+import Chart from 'chart.js/auto';
 import './App.scss';
 
 function App() {
+  const [canvas, setCanvas] = useState<HTMLCanvasElement | null>(null);
+
+  useEffect(() => {
+    if (canvas) {
+      new Chart(canvas, {
+        type: 'line',
+        data: {
+          labels: [1, 2, 3, 4, 5, 6],
+          datasets: [
+            {
+              label: 'Хищник',
+              data: [2, 3, 4, 2, 2, 5],
+              borderColor: 'red',
+            },
+            {
+              label: 'Жертва',
+              data: [5, 4, 3, 5, 5, 2],
+              borderColor: 'blue',
+            },
+          ],
+        },
+      });
+    }
+  }, [canvas]);
+
   return (
     <>
       <h1>Визуализация модели Лотки–Вольтерры</h1>
+      <canvas ref={setCanvas}></canvas>
     </>
   );
 }
